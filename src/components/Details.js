06971bee56352aa8ba91220dab58fb38641a13c3
nlf4 <nlfurtado4@gmail.com>
2021-07-26 17:59:57 -0400

    details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import {Card, CardText, CardBody, CardTitle } from 'reactstrap';
 
 const Details = () => {
   return (
-    <p>Details</p>
+    <div className="details">
+        <Card className="ceremony-details">
+            <CardBody>
+                <div class="body-text">
+                <CardTitle>Our Wedding</CardTitle>
+                <CardText id="detail-label">Date:</CardText>
+                <CardText>Sunday, May 15, 2022</CardText>
+                <CardText id="detail-title">Ceremony</CardText>
+                <CardText id="detail-label">Time:</CardText>
+                <CardText>4:00pm</CardText>
+                <CardText id="detail-title">Reception</CardText>
+                <CardText id="detail-label">Time:</CardText>
+                <CardText>6:00pm</CardText>
+                <CardText id="detail-label">Location:</CardText>
+                <CardText>Westerville, Ohio</CardText>
+                </div>
+            </CardBody>
+        </Card>
+    </div>
   )
 }
 
