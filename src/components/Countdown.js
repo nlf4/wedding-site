7bc53b69df1fce2e164e96a95d7be3bc27de159f
nlf4 <nlfurtado4@gmail.com>
2021-08-14 22:51:08 -0400

    registry

---
+++ src/components/Countdown.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Countdown = () => {
+    weddingDate = new Date(2022, 4, 26, 12, 0);
+  return (
+    <div class="container"> 
+       <p>Countdown</p>
+    </div>
+   
+  )
+}
+
+export default Countdown
--- src/components/Registry.js
+++ src/components/Registry.js
@@ -1,13 +1,49 @@
 
 import React from 'react';
+import amazon from '../images/amazon.png'
+import cratebarrel from '../images/crate_and_barrel.png'
+import background from '../images/lindsayzach.jpg'
+<img src={background} alt="Lindsay and Zach" />
 
 const Registry = () => {
   return (
-    <div class="registry-container"> 
-       <p>Registry</p>
+    <div class="registry-container">
+      <h3>Wedding Registry</h3>
+      <p>We are registered at Amazon and Crate&Barrel</p>
+      <div id="reg-links">
+        <div>
+            <a href="http://www.amazon.com">
+            <img class="reg-image" src={amazon} alt="Amazon" />
+          </a>
+          </div>
+          <div>
+            <a href="http://www.crateandbarrel.com">
+            <img class="reg-image" src={cratebarrel} alt="Crate&Barrel" />
+              
+            </a>
+          </div>
+      </div>
+       
+
+        <p>Or you can donate to one of the charities listed below!</p>
+        
+      
     </div>
-   
   )
 }
-
 export default Registry
+
+
+{/* <h3>Wedding Registry</h3>
+         <p>We are registered at Amazon and CrateandBarrel, or you can donate to one of the charities listed below!</p>
+      
+            <a href="http://www.amazon.com/registry/wedding/3486CYEQP58UF">
+                <img src="/img/amazon.png">
+            </a>
+      
+
+ 
+            <a href="http://www.crateandbarrel.com/Gift-Registry/Leslie-Verploegen-and-Eric-Ferraiuolo/r5038140">
+                <img src="/img/crate_and_barrel.png">
+            </a>
+       */}
